434f8c9661acecd27d2788af3c81bf9e5dc7e0fd
Lana <lanadrahrepus2552@gmail.com>
2020-12-01 22:13:32 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'max-len': [
+      'warn',
+      {
+        code: 120,
+      },
+    ],
+  },
+};
